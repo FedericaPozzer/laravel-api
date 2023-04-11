3491f8d71da746bf9ee2e9e204b88e121d8983c3
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-11 20:56:43 +0200

    ugly layout done - all is finally working good

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,23 +2,30 @@
 
 @section("content")
 
+<h1 class=my-5>Projects</h1>
+
 <table class="table table-warning table-striped">
     <thead>
         <tr>
         <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Title</th>
+        <th scope="col">Abstract</th>
+        <th scope="col">Details</th>
         </tr>
     </thead>
     <tbody>
+        @forelse($projects as $project)
         <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
+        <th scope="row">{{$project->id}}</th>
+        <td>{{$project->title}}</td>
+        <td>{{$project->getAbstract()}}</td>
+        <td><a href="{{route("admin.projects.show", $project)}}">READ</a></td>
         </tr>
+        @empty
+        @endforelse
     </tbody>
 </table> 
+
+{{$projects->links()}}
 
 @endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,6 +2,15 @@
 
 @section("content")
 
-@dump($project)
+<h2 class="my-5">{{$project->title}}</h2>
+
+<section class="row">
+    <div class="col-8">
+        <img src="{{$project->image}}" alt="img">
+    </div>
+    <div class="col-4">
+        <p>{{$project->text}}</p>
+    </div>
+</section>
 
 @endsection
