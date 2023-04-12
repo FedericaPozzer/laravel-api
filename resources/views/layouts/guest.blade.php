f15a59403e069a62ae1c508cf6b6f625bebcf8ee
Federica Pozzer <federica.pozzer@gmail.com>
2023-04-12 17:12:00 +0200

    setted css guest / css front (not customized yet)

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/front.js'])
 </head>
 
 <body class="bg-primary text-dark bg-opacity-25">
